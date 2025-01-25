693a9c699235c7b8b0a302d0837ef1104de2170e
isabellapulte <isapulte@gmail.com>
2025-01-25 13:47:05 -0500

    reredo Intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,34 @@
 package frc.robot.subsystems;
 
-public class Intake {
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class Intake extends SubsystemIO{
+
+    private TalonFX m_pivot;
+    private TalonFX m_roller;
+
+    public Intake() {
+        m_pivot = new TalonFX(IntakeConstants.kpivotId);
+        m_roller = new TalonFX(IntakeConstants.kroller);
+    }
+
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stop'");
+    }
+
+    @Override
+    public boolean checkSystem() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkSystem'");
+    }
+
+    @Override
+    public void outputTelemetry() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'outputTelemetry'");
+    }
 
 }
