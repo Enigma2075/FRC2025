00d1044b3a9e2917dacf3042a4244e4747a31270
isabellapulte <isapulte@gmail.com>
2025-01-25 15:42:39 -0500

    Updated Intake

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,11 +1,14 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 public class Intake extends SubsystemIO{
 
     private TalonFX m_pivot;
     private TalonFX m_roller;
+
+    private final PositionVoltage m_IntakeRequest= new PositionVoltage(0);
 
     public Intake() {
         m_pivot = new TalonFX(IntakeConstants.kpivotId);
@@ -23,10 +26,40 @@ public class Intake extends SubsystemIO{
             this.angle = angle;
         }
     } 
+public static class PeriodicIO {
+    State requestedState = State.CLIMBREADY;
+    double enc = 0;
 
+    double lastPosition = Double.MIN_VALUE;
+}
 
+private final PeriodicIO m_PeriodicIO = new PeriodicIO();
 
-    @Override
+@Override
+public void readPeriodicInputs() {
+    m_PeriodicIO.enc = m_pivot.getPosition().getValueAsDouble();
+    m_PeriodicIO.enc = m_roller.getPosition().getValueAsDouble();
+}
+
+private void writeIntake(double position) {
+    if(m_PeriodicIO.lastPosition!= position) {
+        m_pivot.setControl(m_IntakeRequest.withPosition(position));
+        m_roller.setControl(m_IntakeRequest.withPosition(position));
+        m_PeriodicIO.lastPosition = position;
+    }
+}
+
+@Override
+public void writePeriodicOutputs() {
+    switch(m_PeriodicIO.requestedState)
+    {
+        case CLIMBREADY: 
+            writeIntake(0);
+
+    }
+}
+
+     @Override
     public void stop() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'stop'");
