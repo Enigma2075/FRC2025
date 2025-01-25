dd803396229ef5ac0a0ec31c38bba9b2b597f611
HELEN248HS <144963247+HELEN248HS@users.noreply.github.com>
2025-01-25 13:47:17 -0500

    Climb (again)

--- src/main/java/frc/robot/subsystems/Climb.java
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -1,5 +1,34 @@
 package frc.robot.subsystems;
 
-public class Climb {
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class Climb extends SubsystemIO{
+
+    private TalonFX m_Back;
+    private TalonFX m_Front;
+
+    public Climb() {
+        m_Back = new TalonFX(ClimbConstants.kBackId);
+        m_Front = new TalonFX(ClimbConstants.kFrontId);
+    }
+
+
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stop'");
+    }
+
+    @Override
+    public boolean checkSystem() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkSystem'");
+    }
+
+    @Override
+    public void outputTelemetry() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'outputTelemetry'");
+    }
 
 }
