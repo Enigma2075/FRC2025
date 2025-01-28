5a567188011b45760d8e35e2e5c7535a4f4ce0f4
Yelynn-Heo <floralynn927@naver.com>
2025-01-27 19:37:45 -0500

    extended some subsystmes :)

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,5 +1,22 @@
 package frc.robot.subsystems;
 
-public class Arm {
+public class Arm extends SubsystemIO{
     
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stop'");
+    }
+
+    @Override
+    public boolean checkSystem() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkSystem'");
+    }
+
+    @Override
+    public void outputTelemetry() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'outputTelemetry'");
+    }
 }
--- src/main/java/frc/robot/subsystems/Wrist.java
+++ src/main/java/frc/robot/subsystems/Wrist.java
@@ -1,5 +1,22 @@
 package frc.robot.subsystems;
 
-public class Wrist {
+public class Wrist extends SubsystemIO{
     
+    @Override
+    public void stop() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'stop'");
+    }
+
+    @Override
+    public boolean checkSystem() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkSystem'");
+    }
+
+    @Override
+    public void outputTelemetry() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'outputTelemetry'");
+    }
 }
