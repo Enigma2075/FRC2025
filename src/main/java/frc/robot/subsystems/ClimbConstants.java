81a08177ec41f2e1b417664693ce67ed0bf12c44
Yelynn-Heo <floralynn927@naver.com>
2025-02-01 13:01:14 -0500

    gear ratio for climb

--- src/main/java/frc/robot/subsystems/ClimbConstants.java
+++ src/main/java/frc/robot/subsystems/ClimbConstants.java
@@ -4,5 +4,8 @@ public class ClimbConstants {
     public static final int kBackId = 14;
     public static final int kFrontId = 2;
 
+    public static final double kGearRatio = 3.75;
+    
+
     public static final Climb Climb = new Climb();
 }
