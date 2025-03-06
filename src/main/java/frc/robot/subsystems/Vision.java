a8c042c1e7a2851e1791582379425edd555fc2e6
Yelynn-Heo <floralynn927@naver.com>
2025-03-05 20:56:30 -0500

    vision stuff

---
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems;
+
+public class Vision extends SubsystemIO {
+    public Vision() {
+        
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public boolean checkSystem() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'checkSystem'");
+    }
+
+    @Override
+    public void outputTelemetry() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'outputTelemetry'");
+    }
+
+    @Override
+    public void readPeriodicInputs() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'readPeriodicInputs'");
+    }
+
+    @Override
+    public void writePeriodicOutputs() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'readPeriodicOutputs'");
+    }
+}
