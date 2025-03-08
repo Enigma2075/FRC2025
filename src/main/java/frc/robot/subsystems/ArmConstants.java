66d59b7a9fd6f62f2fd2d6761ec53619b09a5b61
Yelynn-Heo <floralynn927@naver.com>
2025-03-08 15:19:38 -0500

    arm enc

--- src/main/java/frc/robot/subsystems/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/ArmConstants.java
@@ -35,6 +35,6 @@ public class ArmConstants {
     public static final double kMotionMagicAcceleration = (kMaxRPS * .5)/.8;
     public static final double kMotionMagicJerk = 0;
 
-    public static final double kMagnetOffset = RobotConstants.kPracticeBot ? -0.50244140625 : -0.46337890625;
+    public static final double kMagnetOffset = RobotConstants.kPracticeBot ? -0.49658203125 : -0.46337890625;
 
 }
