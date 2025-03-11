a20c6f360bf1879a36eef87cc8d7094bd97afd0b
EddieDL <mdressel@gmail.com>
2025-03-10 21:54:41 -0400

    Updates

--- src/main/java/frc/robot/subsystems/DriveTrainConstants.java
+++ src/main/java/frc/robot/subsystems/DriveTrainConstants.java
@@ -30,8 +30,8 @@ public class DriveTrainConstants {
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
-        .withKP(0.1).withKI(0).withKD(0)
-        .withKS(0).withKV(0.124);
+        .withKP(0.15959).withKI(0).withKD(0)
+        .withKS(0.16682).withKV(0.12204).withKA(.018688);
 
     // The closed-loop output type to use for the steer motors;
     // This affects the PID/FF gains for the steer motors
@@ -55,7 +55,19 @@ public class DriveTrainConstants {
 
     // Initial configs for the drive and steer motors and the azimuth encoder; these cannot be null.
     // Some configs will be overwritten; check the `with*InitialConfigs()` API documentation.
-    private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration();
+    private static final TalonFXConfiguration driveInitialConfigs = new TalonFXConfiguration()
+        .withCurrentLimits(
+            new CurrentLimitsConfigs()
+                // Swerve drive requires a lot of torque output, so we can set a relatively high
+                // stator current limit to help avoid brownouts without impacting performance.
+                .withStatorCurrentLimit(Amps.of(120))
+                .withStatorCurrentLimitEnable(true)
+                .withSupplyCurrentLimit(Amps.of(80))
+                .withSupplyCurrentLowerLimit(40)
+                .withSupplyCurrentLowerTime(.75)
+                .withSupplyCurrentLimitEnable(true)
+        );
+
     private static final TalonFXConfiguration steerInitialConfigs = new TalonFXConfiguration()
         .withCurrentLimits(
             new CurrentLimitsConfigs()
